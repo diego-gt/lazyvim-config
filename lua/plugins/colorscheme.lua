bcb24d363e09432a70d11245ee7f456888ac940f
Diego Garza <dgarza@reynera.com.mx>
2024-10-28 09:55:04 -0600

    sync

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,11 @@
+return {
+  { "rose-pine/neovim", name = "rose-pine", opts = {
+    variant = "dawn",
+  } },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
